641ec55abad761a4ded39b3fba586ee2963f3d30
cld53 <43147730+cld53@users.noreply.github.com>
2019-03-13 08:58:57 -0500

    Update parse.h

--- Project2/parse.h
+++ Project2/parse.h
@@ -1,8 +1,8 @@
 #ifndef parse
 #define parse
 	
-	void parse_numbers(int intArr[], char buffer[], char inputPathArr[]);
-	void clear_path(int inputPathArr[], max_Size);
+	void parse_numbers(int inputPathArr[], char buffer[], char messageIn[]);
+	void clear_path(int inputPathArr[], int max_Size);
 	void clear_string(char * string, int max_Size);
 
 	
