55a4605ecf42b72c0c622522cd8289608215869e
cld53 <43147730+cld53@users.noreply.github.com>
2019-03-17 14:33:47 -0500

    Delete parse.h

--- parse.h
+++
@@ -1,8 +0,0 @@
-#ifndef parse
-#define parse
-	
-	void parse_message(char buffer[], int * mode, char username[], char password[], char destination[], char body[]);
-	void clear_path(int inputPathArr[], int max_Size);
-	void clear_string(char * string, int max_Size);
-
-#endif
