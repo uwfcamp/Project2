6c461c67b9b224cc60a7fb494bbe51cb7f3d066f
cld53 <43147730+cld53@users.noreply.github.com>
2019-03-17 14:33:40 -0500

    Delete s_menu_funct.h

--- s_menu_funct.h
+++
@@ -1,8 +0,0 @@
-#ifndef s_menu_funct
-#define s_menu_funct
-	
-	void broadcast_message(client_list_t *clientList, int sender_socket, char *message, char *sender);
-	void private_message(client_list_t *clientList, char *message, char *destination, char *sender);
-	void log_into_group(char *username, char *body);
-
-#endif
