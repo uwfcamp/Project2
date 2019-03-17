2b245e58e05cda9f200179c3ffef7fec2c5b6e15
cld53 <43147730+cld53@users.noreply.github.com>
2019-03-17 14:34:01 -0500

    Delete s_login_funct.h

--- s_login_funct.h
+++
@@ -1,7 +0,0 @@
-#ifndef s_login_funct
-#define s_login_funct
-	
-	int login_user(char *username, char *password, client_list_t *client);
-	int register_user(char *username, char *password, client_list_t *client);
-	
-#endif
