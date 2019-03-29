cf715763c7bf7bc39deff44b9b14e5ff7ecdd48d
cld53 <43147730+cld53@users.noreply.github.com>
2019-03-29 13:18:56 -0500

    Update s_admin_funct.h

--- Project2/s_admin_funct.h
+++ Project2/s_admin_funct.h
@@ -1,3 +1,8 @@
+//Curtis Duvall, Nathan Wilkins, and Julian Thayer
+//Systems and Networking II
+//Project 2
+//s_admin_funct.h
+
 #ifndef s_admin_funct
 #define s_admin_funct
 	void admin_login(char * username, char * password, client_list_t *current, admin_account_t * admin);
