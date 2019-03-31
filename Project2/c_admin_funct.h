a8c653c13ae59b47f5f6e204bc1b46db36c51376
uwfcamp <naw17@students.uwf.edu>
2019-03-31 18:41:17 -0500

    Update c_admin_funct.h

--- Project2/c_admin_funct.h
+++ Project2/c_admin_funct.h
@@ -6,9 +6,16 @@
 #ifndef c_admin_funct
 #define c_admin_funct
 	
+	// will promt the user for administrator credentials
 	void admin_login(server_t *server);
+
+	// will print a menu of administrative options, and allow the user to choose one
 	void admin_menu(server_t *server, admin_account_t *admin);
+
+	// will prompt the user for the name of another user, and ban that user from the chat
 	void ban_user(server_t *server, admin_account_t *admin);
+
+	// will prompt the user for the name of another user, and kick that user out
 	void kick_user(server_t *server, admin_account_t *admin);
 	
 #endif
