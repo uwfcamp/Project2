be9a3b0fd9c5d52c701eb6c7d8255383073c17ca
uwfcamp <naw17@students.uwf.edu>
2019-03-31 18:56:10 -0500

    Update server_framework.h

--- Project2/server_framework.h
+++ Project2/server_framework.h
@@ -11,8 +11,11 @@
 	#include "s_menu_funct.h"
 	#include "s_login_funct.h"
 	
+	// add a new connection to the link list, located at the designated socket
 	void new_connection(client_list_t *clientList, int socket);
+	// remove the connection located at the designated socket
 	void remove_connection(client_list_t *clientList, int target_socket);
+	// thread function to let the main thread know to send a message twice every TIMEOUT_INTERVAL
 	void *ping_connections(void *vargp);
 
 #endif
