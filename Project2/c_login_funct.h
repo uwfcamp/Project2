5add43391e24a38c8283720959607ad9df46eaf2
uwfcamp <naw17@students.uwf.edu>
2019-03-31 19:37:05 -0500

    Update c_login_funct.h

--- Project2/c_login_funct.h
+++ Project2/c_login_funct.h
@@ -8,9 +8,13 @@
 	#include "Definitions.h"
 	#include "client_framework.h"
 
+	// print the initial login menu, and allow the user to choose what they want to do
 	int login_menu(server_t *server);
+	// promt the user for a username and password, and send them to the server for registration
 	void registration_input(server_t *server);
+	// promt the user for a username and password, and send them to the server to validate
 	void login_input(server_t *server);
+	// send a mssage to the server to ask if the user is banned or kicked out
 	int is_banned_or_kicked(server_t *server);
 
 #endif
