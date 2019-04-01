23004a7ef1d1cd59085de804ef7c0719da27f595
uwfcamp <naw17@students.uwf.edu>
2019-03-31 19:11:17 -0500

    Update s_admin_funct.h

--- Project2/s_admin_funct.h
+++ Project2/s_admin_funct.h
@@ -5,7 +5,12 @@
 
 #ifndef s_admin_funct
 #define s_admin_funct
+
+	// will verify the adminstrative credentials from the client, and let them know if they are valid
 	void admin_login(char * username, char * password, client_list_t *current, admin_account_t * admin);
+	// will ban a user from the server, and let them know that they are banned
 	void ban_user(char * destination, client_list_t * clientList, client_list_t * current);
+	// will kick a user off the server, and let them know that they are kicked out
 	void kick_user(char * destination, client_list_t * clientList, client_list_t * current);
+
 #endif
