805a4b4ddb3250bef78cf081ba4d81d1af024f61
cld53 <43147730+cld53@users.noreply.github.com>
2019-04-03 08:40:44 -0500

    Update Definitions.h

--- Project3/Definitions.h
+++ Project3/Definitions.h
@@ -1,4 +1,25 @@
 #ifndef Definitions
 #define Definitions
+  //libraries
+  #include <stdio.h>
+	#include <sys/socket.h>
+	#include <sys/types.h>
+	#include <unistd.h>
+	#include <stdlib.h>
+	#include <errno.h>
+	#include <netinet/in.h>
+	#include <string.h>
+	#include <fcntl.h>
+	#include <time.h>
+	#include <arpa/inet.h>
+	#include <pthread.h>
+	#include <stdlib.h>
+	#include <semaphore.h>
+	#include <sys/stat.h>
+  #include <sys/time.h>
+
+  //Global Definitions
+
+  //Structures
 
 #endif
