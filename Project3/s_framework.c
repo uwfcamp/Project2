9cb2d3cb8a41197205145b3ed833a4e717e1283d
cld53 <43147730+cld53@users.noreply.github.com>
2019-04-03 08:37:58 -0500

    Create s_framework.c

---
+++ Project3/s_framework.c
@@ -0,0 +1,14 @@
+#include "Definitions.h"
+#include "s_framework.h"
+int main(void) {
+
+//startup
+
+//option to let user set up IP or use local network as default
+
+//When a client connects, a new thread is created to handle each new connection.
+
+//shutdown
+
+    return 0;
+}
